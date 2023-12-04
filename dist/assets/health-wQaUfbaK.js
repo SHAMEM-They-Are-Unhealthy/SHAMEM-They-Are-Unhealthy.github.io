fd065a6e62124e514a67bac14ba5649b0b805bea
TheUltimateChickenLord <Alexander.B_2023_Lon@outlook.com>
2023-12-04 19:27:55 +0000

    Adding dist

--- dist/assets/health-wQaUfbaK.js
+++
@@ -1,1 +0,0 @@
-import"./NavBar-FCUqYbo2.js";const e=document.querySelector("#range"),o=document.querySelector("#currentAge");e.addEventListener("input",n=>{const t=Number(n.target.value);o.textContent=t+" years old";const r=t/e.max*100;e.style.background=`linear-gradient(to right, #f50 ${r}%, #ccc ${r}%)`,e.style.setProperty("--thumb-rotate",`${t/100*2160}deg`)});
