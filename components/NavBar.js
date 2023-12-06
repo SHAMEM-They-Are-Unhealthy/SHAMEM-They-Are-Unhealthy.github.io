572551c1782cad4975f6443cc97e4046fa13372c
matt <matthew.n_2023_lon@outlook.com>
2023-12-06 16:19:15 +0000

    changed navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,65 +1,75 @@
-import "./nav.css"
+import "./nav.css";
 
 class NavBar extends HTMLElement {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    connectedCallback() {
-        this.innerHTML = `
+  connectedCallback() {
+    this.innerHTML = `
         <header class="header">
-            <nav class="navbar">
-            <a href="/" class="nav-logo"><img id="logo-img" style="width: 100px; height:auto;"/></a>
-            <ul class="nav-menu">
-                <li class="nav-item">
-                    <a href="/" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/diet/" class="nav-link">Diet</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/contact/" class="nav-link">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/workout/" class="nav-link">Workout</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/health/" class="nav-link">Health</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/benefits/" class="nav-link">Benefits</a>
-                </li>
-            </ul>
-            <div class="hamburger">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </div>
-            </nav>
-        </header>
-        <div style="height: 55px;"></div>
-        `;
-
-    }
+  <nav class="navbar">
+    <a href="/" class="nav-logo"
+      ><img id="logo-img" style="width: 100px; height: auto"
+    /></a>
+    <ul class="nav-menu">
+      <a href="/" class="nav-link">
+        <li class="nav-item">Home</li>
+      </a>
+      <a href="/health-info/" class="nav-link">
+        <li class="nav-item health-info">
+          Health Info
+          <span class="dropdown-icon">
+            <i class="fa-solid fa-caret-down"></i>
+          </span>
+          <ul class="drop-down">
+            <a href="/health/">
+              <li>Health</li>
+            </a>
+            <a href="/diet/">
+              <li>Diet</li>
+            </a>
+            <a href="/benefits/">
+              <li>Benefits</li>
+            </a>
+          </ul>
+        </li>
+      </a>
+      <a href="/workout/" class="nav-link">
+        <li class="nav-item">Workout</li>
+      </a>
+      <a href="/contact/" class="nav-link">
+        <li class="nav-item">Contact</li>
+      </a>
+    </ul>
+    <div class="hamburger">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </div>
+  </nav>
+</header>
+<div style="height: 55px"></div>`;
+  }
 }
 
-customElements.define('nav-bar', NavBar);
+customElements.define("nav-bar", NavBar);
 
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector(".nav-menu");
-    const navLink = document.querySelectorAll(".nav-link");
-  
-    hamburger.addEventListener("click", mobileMenu);
-    navLink.forEach(n => n.addEventListener("click", closeMenu));
-  
-    function mobileMenu() {
-        hamburger.classList.toggle("active");
-        navMenu.classList.toggle("active");
-    }
-  
-    function closeMenu() {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+  const navLink = document.querySelectorAll(".nav-link");
+
+  hamburger.addEventListener("click", mobileMenu);
+  navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+
+  function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
 });
