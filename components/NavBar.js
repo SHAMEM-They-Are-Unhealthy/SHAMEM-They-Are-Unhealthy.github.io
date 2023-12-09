4cf7d48a2ba599a1efb599ed2f404d1770af7d95
TheUltimateChickenLord <Alexander.B_2023_Lon@outlook.com>
2023-12-09 18:36:51 +0000

    small format changes

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,75 +1,74 @@
 import "./nav.css";
 
 class NavBar extends HTMLElement {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  connectedCallback() {
-    this.innerHTML = `
-        <header class="header">
-  <nav class="navbar">
-    <a href="/" class="nav-logo"
-      ><img id="logo-img" style="width: 100px; height: auto"
-    /></a>
-    <ul class="nav-menu">
-      <a href="/" class="nav-link">
-        <li class="nav-item">Home</li>
-      </a>
-      <a href="/health-info/" class="nav-link">
-        <li class="nav-item health-info">
-          Health Info
-          <span class="dropdown-icon">
-            <i class="fa-solid fa-caret-down"></i>
-          </span>
-          <ul class="drop-down" style="text-align: left;">
-            <a href="/health/">
-              <li>Health</li>
+    connectedCallback() {
+        this.innerHTML = `
+<header class="header">
+    <nav class="navbar">
+        <a href="/" class="nav-logo"
+            ><img id="logo-img" style="width: 100px; height: auto"
+        /></a>
+        <ul class="nav-menu">
+            <a href="/" class="nav-link">
+                <li class="nav-item">Home</li>
             </a>
-            <a href="/diet/">
-              <li>Diet</li>
+            <li class="nav-item health-info">
+                Health Info
+                <span class="dropdown-icon">
+                    <i class="fa-solid fa-caret-down"></i>
+                </span>
+                <ul class="drop-down" style="text-align: left;">
+                    <a href="/health/">
+                        <li class="nav-item">Health</li>
+                    </a>
+                    <a href="/diet/">
+                        <li class="nav-item">Diet</li>
+                    </a>
+                    <a href="/benefits/">
+                        <li class="nav-item">Benefits</li>
+                    </a>
+                </ul>
+            </li>
             </a>
-            <a href="/benefits/">
-              <li>Benefits</li>
+            <a href="/workout/" class="nav-link">
+                <li class="nav-item">Workout</li>
             </a>
-          </ul>
-        </li>
-      </a>
-      <a href="/workout/" class="nav-link">
-        <li class="nav-item">Workout</li>
-      </a>
-      <a href="/contact/" class="nav-link">
-        <li class="nav-item">Contact</li>
-      </a>
-    </ul>
-    <div class="hamburger">
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
-    </div>
-  </nav>
+            <a href="/contact/" class="nav-link">
+                <li class="nav-item">Contact</li>
+            </a>
+        </ul>
+        <div class="hamburger">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </nav>
 </header>
 <div style="height: 55px"></div>`;
-  }
+    }
 }
 
 customElements.define("nav-bar", NavBar);
 
 document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".nav-menu");
-  const navLink = document.querySelectorAll(".nav-link");
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    const navLink = document.querySelectorAll(".nav-link");
 
-  hamburger.addEventListener("click", mobileMenu);
-  navLink.forEach((n) => n.addEventListener("click", closeMenu));
+    hamburger.addEventListener("click", mobileMenu);
+    navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
-  function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  }
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
 
-  function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  }
+    function closeMenu() {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
 });
--- contact/contact.js
+++ contact/contact.js
@@ -7,38 +7,38 @@ const submit = document.querySelector(".submit-btn");
 const alert = document.querySelector(".alert");
 
 submit.addEventListener("click", (e) => {
-  let inputChecker = true;
-  e.preventDefault();
-  checkEmpty(fname);
-  checkEmpty(email);
-  checkEmpty(subject);
-  checkEmpty(enquiry);
-  function checkEmpty(input) {
-    if (!input.value) {
-      displayAlert("Input field empty", "red");
-      inputChecker = false;
+    let inputChecker = true;
+    e.preventDefault();
+    checkEmpty(fname);
+    checkEmpty(email);
+    checkEmpty(subject);
+    checkEmpty(enquiry);
+    function checkEmpty(input) {
+        if (!input.value) {
+            displayAlert("Input field empty", "red");
+            inputChecker = false;
+        }
     }
-  }
-  if (inputChecker) {
-    displayAlert("Enquiry Submitted", "green");
-    clearInputs(fname);
-    clearInputs(lname);
-    clearInputs(email);
-    clearInputs(enquiry);
-    subject.selectedIndex = subject.options[0];
-  }
+    if (inputChecker) {
+        displayAlert("Enquiry Submitted", "green");
+        clearInputs(fname);
+        clearInputs(lname);
+        clearInputs(email);
+        clearInputs(enquiry);
+        subject.selectedIndex = subject.options[0];
+    }
 
-  function displayAlert(text, style) {
-    alert.textContent = text;
-    alert.classList.add(`alert-${style}`);
-    // remove alert
-    setTimeout(() => {
-      alert.textContent = "";
-      alert.classList.remove(`alert-${style}`);
-    }, 3000);
-  }
+    function displayAlert(text, style) {
+        alert.textContent = text;
+        alert.classList.add(`alert-${style}`);
+        // remove alert
+        setTimeout(() => {
+            alert.textContent = "";
+            alert.classList.remove(`alert-${style}`);
+        }, 3000);
+    }
 
-  function clearInputs(input) {
-    input.value = "";
-  }
+    function clearInputs(input) {
+        input.value = "";
+    }
 });
