8ad22bf057de9b5222a9ed4d938205ab906a8220
TheUltimateChickenLord <Alexander.B_2023_Lon@outlook.com>
2023-12-13 14:06:49 +0000

    Adding dist

---
+++ assets/quiz-89piAP4s.js
@@ -0,0 +1,4 @@
+import"./NavBar-8kjRtyNB.js";const i=e=>{var t;return((t=document.cookie.match("(^|;)\\s*"+e+"\\s*=\\s*([^;]+)"))==null?void 0:t.pop())||""};$(document).ready(function(){i("session-id")!=null?$.get("https://shamem.glitch.me/login_test",{sessionID:i("session-id")}).done(function(e,t){document.cookie="session-id="+e+"; path=/",document.getElementById("login").innerHTML=`<h3>Are you ready?</h3>
+<div class="a-container" style="text-align: center;">
+    <a class="btn" href="/quiz/health-quiz/">Start the Quiz</a>
+</div>`}):window.location="/login/"});
